c492b6717210a7cf37e12a6ca23c8011fc21e1b8
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2021-12-11 09:44:09 -0300

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/jsx-runtime'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 13,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	},
+	'settings': {
+		'react': {
+			'version': 'detect'
+		}
+	}
+}
