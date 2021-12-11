23a5b2ba95045fa1d4c00c24fa858e61d98ec2d5
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2021-12-11 13:13:48 -0300

    Add presentation section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import { Button } from './components/Button'
+import { useEffect, useRef } from 'react'
 import { Header } from './components/Header'
 
 function App() {
-  const [counter, setCounter] = useState(0)
+  const nameInputRef = useRef<HTMLInputElement>(null)
+  useEffect(() => {
+    nameInputRef.current?.focus()
+  }, [])
   return (
-    <div className="text-center min-h-screen text-white text-sm px-28 py-20">
+    <div className="min-h-screen text-white text-sm px-28 py-20">
       <Header />
+      <main className="w-full mt-20">
+        <h1 className="text-7xl font-bold text-left">
+          Olá,{' '}
+          <input
+            ref={nameInputRef}
+            type="text"
+            className="font-bold max-w-md bg-transparent border-b-4 border-white border-solid focus-within:outline-none"
+            style={{ color: '#F72585' }}
+          />{' '}
+          !
+        </h1>
+        <p className="text-4xl font-medium mt-8">
+          Meu nome é <span style={{ color: '#F72585' }}>Cleberson Jr.</span> e
+          desenvolvo para a <span style={{ color: '#F72585' }}>web</span>.
+        </p>
+      </main>
     </div>
   )
 }
