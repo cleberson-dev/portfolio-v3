78dbcc3bae2c4d7546097a48c26a33cfd2d4b98c
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2021-12-12 22:38:27 -0300

    Take nav items out of component's props

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,12 @@ import { Logo } from './Logo'
 import { Navbar } from './Navbar'
 import { SocialMediaProps, SociaMedia } from './SocialMedia'
 
+const navbar = [
+  { link: '#', title: 'sobre mim' },
+  { link: '#', title: 'techs' },
+  { link: '#', title: 'projetos' },
+]
+
 const socials: SocialMediaProps[] = [
   { type: 'github', link: '' },
   { type: 'linkedin', link: '' },
@@ -12,13 +18,7 @@ export const Header = (): JSX.Element => {
   return (
     <header className="flex justify-between items-center">
       <Logo />
-      <Navbar
-        items={[
-          { link: '#', title: 'sobre mim' },
-          { link: '#', title: 'techs' },
-          { link: '#', title: 'projetos' },
-        ]}
-      />
+      <Navbar items={navbar} />
       <div className="flex">
         {socials.map((social) => (
           <SociaMedia key={social.type} {...social} />
