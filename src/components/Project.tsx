9b00fec91a6b9143d826475ab578e80db8716495
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2021-12-20 09:45:10 -0300

    Create Project component

---
+++ src/components/Project.tsx
@@ -0,0 +1,26 @@
+type ProjectProps = {
+  cover: string
+  name: string
+  description: string
+  techs: string[]
+}
+
+export const Project = ({
+  cover,
+  name,
+  description,
+  techs,
+}: ProjectProps): JSX.Element => {
+  return (
+    <div className="w-64">
+      <img
+        src={cover}
+        alt="Project cover"
+        className="w-full h-64 object-cover rounded-full"
+      />
+      <p className="font-bold text-2xl">{name}</p>
+      <p className="text-base">{description}</p>
+      <div></div>
+    </div>
+  )
+}
--- src/sections/Projects.section.tsx
+++ src/sections/Projects.section.tsx
@@ -1,19 +1,23 @@
+import { Project } from '../components/Project'
+
+const projects = [
+  {
+    name: 'Poll It',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et lacus at urna imperdiet egestas nec dignissim massa. Vivamus semper rutrum consequat. ',
+    techs: [],
+    cover:
+      'https://unsplash.com/photos/gySMaocSdqs/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjQwMDAxNDA1&force=true&w=640',
+  },
+]
+
 export const ProjectsSection = () => {
   return (
     <section>
       <h1 className="font-bold text-3xl lowercase">Projects</h1>
 
       <div className="grid grid-cols-3 gap-x-3 mt-20">
-        <div>
-          <img src="" alt="Project cover" />
-          <p className="font-bold text-2xl">Poll It</p>
-          <p className="text-base">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et
-            lacus at urna imperdiet egestas nec dignissim massa. Vivamus semper
-            rutrum consequat.
-          </p>
-          <div></div>
-        </div>
+        <Project {...projects[0]} />
       </div>
     </section>
   )
