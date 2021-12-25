1f60159f640cd12a3e2c2e581c2b35aefbc1f0aa
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2021-12-25 17:14:17 -0300

    Define techs property with TechIconSelector on Project component

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,14 +1,17 @@
+import { AvailableTechs, TechIconSelector } from './TechIconSelector'
+
 type ProjectProps = {
   cover: string
   name: string
   description: string
-  techs: string[]
+  techs: AvailableTechs[]
 }
 
 export const Project = ({
   cover,
   name,
   description,
+  techs,
 }: ProjectProps): JSX.Element => {
   return (
     <div className="w-64">
@@ -19,7 +22,21 @@ export const Project = ({
       />
       <p className="font-bold text-2xl">{name}</p>
       <p className="text-base">{description}</p>
-      <div></div>
+      <div className="grid grid-cols-5 mt-3">
+        {techs.map((tech) => (
+          <div
+            key={tech}
+            className="cursor-pointer w-min opacity-30 hover:opacity-100"
+          >
+            <TechIconSelector
+              tech={tech}
+              color="black"
+              width="30px"
+              height="30px"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
