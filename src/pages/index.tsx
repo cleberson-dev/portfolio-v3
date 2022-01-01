b361316213b856e4d8a8a6df6288755b23bb2a21
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2022-01-01 13:36:40 -0300

    Move font import to document component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { Header } from '../components/Header'
 import { AboutSection } from '../sections/About.section'
 import { MainSection } from '../sections/Main.section'
@@ -7,24 +6,16 @@ import { SkillsSection } from '../sections/Skills.section'
 
 function HomePage(): JSX.Element {
   return (
-    <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <div className="text-white text-sm px-28 py-20">
-        <div className="h-screen flex flex-col justify-between">
-          <Header />
-          <MainSection />
-          <div className="h-32"></div>
-        </div>
-        <SkillsSection />
-        <AboutSection />
-        <ProjectsSection />
+    <div className="text-white text-sm px-28 py-20">
+      <div className="h-screen flex flex-col justify-between">
+        <Header />
+        <MainSection />
+        <div className="h-32"></div>
       </div>
-    </>
+      <SkillsSection />
+      <AboutSection />
+      <ProjectsSection />
+    </div>
   )
 }
 
