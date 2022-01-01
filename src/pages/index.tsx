1f5bf415ff755297f815728fbf87b8c285b66434
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2022-01-01 13:37:49 -0300

    Add title to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { Header } from '../components/Header'
 import { AboutSection } from '../sections/About.section'
 import { MainSection } from '../sections/Main.section'
@@ -6,16 +7,21 @@ import { SkillsSection } from '../sections/Skills.section'
 
 function HomePage(): JSX.Element {
   return (
-    <div className="text-white text-sm px-28 py-20">
-      <div className="h-screen flex flex-col justify-between">
-        <Header />
-        <MainSection />
-        <div className="h-32"></div>
+    <>
+      <Head>
+        <title>cleberson.dev</title>
+      </Head>
+      <div className="text-white text-sm px-28 py-20">
+        <div className="h-screen flex flex-col justify-between">
+          <Header />
+          <MainSection />
+          <div className="h-32"></div>
+        </div>
+        <SkillsSection />
+        <AboutSection />
+        <ProjectsSection />
       </div>
-      <SkillsSection />
-      <AboutSection />
-      <ProjectsSection />
-    </div>
+    </>
   )
 }
 
