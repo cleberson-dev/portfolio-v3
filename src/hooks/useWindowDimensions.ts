827249179903bce19c153ca9fb4e31ea0c19e3c6
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2022-01-01 21:25:55 -0300

    Check for windows existence on useWindowDimensions hook

--- src/hooks/useWindowDimensions.ts
+++ src/hooks/useWindowDimensions.ts
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react'
 
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window
-  return { width, height }
+  const hasWindow = typeof window !== 'undefined'
+
+  return {
+    width: hasWindow ? window.innerWidth : 0,
+    height: hasWindow ? window.innerHeight : 0,
+  }
 }
 
 export default function useWindowDimensions() {
