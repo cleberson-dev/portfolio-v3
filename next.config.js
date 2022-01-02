ba96bb9cc12faf98f084958ce4f4ed3ec3ebbc2c
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2022-01-02 12:19:41 -0300

    Add locales on next config file

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+  i18n: {
+    locales: ['pt-BR', 'en'],
+    defaultLocale: 'pt-BR',
+  },
+}
+
+module.exports = nextConfig
