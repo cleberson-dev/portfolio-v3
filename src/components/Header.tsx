b99a6bd0e3ebaeacd203575ee40ded5c3cced001
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2022-01-02 12:37:48 -0300

    Get translated content on Navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,18 +2,22 @@ import { Logo } from './Logo'
 import { Navbar } from './Navbar'
 import { SociaMedia } from './SocialMedia'
 import socials from '../data/socials.json'
-
-const navbar = [
-  { link: '#', title: 'sobre mim' },
-  { link: '#', title: 'techs' },
-  { link: '#', title: 'projetos' },
-]
+import { useRouter } from 'next/router'
+import { getTranslatedContent } from '../utils'
 
 export const Header = (): JSX.Element => {
+  const { locale } = useRouter()
+  const content = getTranslatedContent(locale)
+
   return (
     <header className="flex justify-center items-center lg:justify-between">
       <Logo />
-      <Navbar items={navbar} />
+      <Navbar
+        items={content.navbar.map((item) => ({
+          title: item.label,
+          link: item.link,
+        }))}
+      />
       <div className="hidden lg:flex">
         {socials.map((social) => (
           <SociaMedia key={social.type} {...social} />
