8e62a27ff34a0b813cb1ba046b140b7fd0b49456
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2022-01-08 12:49:20 -0300

    Put project title on top of the card

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -20,15 +20,16 @@ export const Project = ({
 }: ProjectProps): JSX.Element => {
   return (
     <div className="flex flex-col items-center bg-white text-black py-6 px-10 rounded-md">
+      <p className="w-64 font-bold text-xl text-center lg:text-2xl lg:my-0 lg:mb-4">
+        {name}
+      </p>
       <img
         src={cover}
         alt="Project cover"
         className="w-48 h-48 lg:w-64 lg:h-64 object-cover rounded-full"
       />
-      <p className="w-64 font-bold text-xl text-center my-2 lg:text-2xl lg:text-left lg:my-0 lg:mt-4">
-        {name}
-      </p>
-      <p className="w-64 text-center text-sm lg:text-left lg:text-base">
+
+      <p className="w-64 text-center text-sm lg:text-left lg:text-base lg:mt-4">
         {description}
       </p>
       <div className="w-full grid grid-cols-5 mt-3 mb-5">
@@ -51,7 +52,7 @@ export const Project = ({
           <a
             key={link}
             href={link}
-            className="text-white text-center text-base uppercase w-full py-1 px-2"
+            className="text-white text-center text-base uppercase w-full py-1 px-2 hover:"
             style={{ backgroundColor: '#10002B' }}
           >
             {type}
