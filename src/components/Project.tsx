d4eede8df7bd11896b7e04ee73ca36d6ac3664db
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2022-01-08 12:05:11 -0300

    Fix projects stylings

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -19,19 +19,19 @@ export const Project = ({
   links,
 }: ProjectProps): JSX.Element => {
   return (
-    <div className="flex flex-col items-center lg:block w-96 bg-white text-black py-6 px-10 rounded-md">
+    <div className="flex flex-col items-center bg-white text-black py-6 px-10 rounded-md">
       <img
         src={cover}
         alt="Project cover"
-        className="w-48 h-48 lg:w-full lg:h-64 object-cover rounded-full"
+        className="w-48 h-48 lg:w-64 lg:h-64 object-cover rounded-full"
       />
-      <p className="font-bold text-xl text-center my-2 lg:text-2xl lg:text-left lg:my-0">
+      <p className="w-64 font-bold text-xl text-center my-2 lg:text-2xl lg:text-left lg:my-0 lg:mt-4">
         {name}
       </p>
-      <p className="text-center text-sm lg:text-left lg:text-base">
+      <p className="w-64 text-center text-sm lg:text-left lg:text-base">
         {description}
       </p>
-      <div className="grid grid-cols-5 mt-3">
+      <div className="w-full grid grid-cols-5 mt-3 mb-5">
         {techs.map((tech) => (
           <div
             key={tech}
@@ -46,7 +46,7 @@ export const Project = ({
           </div>
         ))}
       </div>
-      <div className="grid grid-cols-2 gap-x-2">
+      <div className="w-full grid grid-cols-2 gap-x-2">
         {Object.entries(links).map(([type, link]) => (
           <a
             key={link}
